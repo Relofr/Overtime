2a3fb8e969adf05fc58208536e281ad8270394c4
Kyle Hatch <ky13h4tch@gmail.com>
2017-11-13 11:42:30 -0700

    Add twitter bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
+@user = User.create!(
+          email: "test@test.com", 
+          password: "asdfasdf", 
+          password_confirmation: "asdfasdf", 
+          first_name: "kyle", 
+          last_name: "test"
+          )
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+ Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
-puts "100 Posts have been created"
-
+puts "1 Posts have been created"
