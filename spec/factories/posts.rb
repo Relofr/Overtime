3d3fd522a91fc46b3659d58493c9e3f3795c0670
Kyle Hatch <ky13h4tch@gmail.com>
2017-11-27 15:57:03 -0700

    Implemented new index query for showing users posts they created and hiding others they did not create

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -10,4 +10,10 @@ FactoryBot.define do
     rationale "Some more Rationale"
     user
   end
+
+  factory :post_from_other_user, class: "Post" do
+    date Date.yesterday
+    rationale "Some more Rationale"
+    non_authorized_user
+  end
 end
